20b6bfe6cc27001b65724c9f55dfaf49ce17e700
betsy_liu <40143204@gm.nfu.edu.tw>
2016-06-28 07:06:50 +0000

    010作業

---
+++ 010_Syntax/testDefine.php
@@ -0,0 +1,5 @@
+<?php
+  define("test", 123);//先定義test是123，之後testInclude.php用得到
+  echo test, "<br>";
+  
+?>
---
+++ 010_Syntax/testInclude.php
@@ -0,0 +1,7 @@
+<?php
+  include_once("testDefine.php");//只呼叫一次，執行錯誤的話後面的程式可以繼續執行
+  //include("testDefine.php");//可以呼叫很多次
+  //require_once("testDefine.php");//只呼叫一次，執行錯誤的話後面的程式即不繼續執行
+  //require("testDefine.php");//可以呼叫很多次
+  echo test;
+?>
